b4df9fcf27db8b580a625db594428087bfd18fe5
Nilesh-Joshi26 <48581869+Nilesh-Joshi26@users.noreply.github.com>
2026-06-14 18:45:13 +0530

    Return normalized customer inquiry matches

--- src/main/java/com/hdfcbank/offerengine/domain/port/out/OfferRecordRepositoryPort.java
+++ src/main/java/com/hdfcbank/offerengine/domain/port/out/OfferRecordRepositoryPort.java
@@ -1,11 +1,11 @@
 package com.hdfcbank.offerengine.domain.port.out;
 
 import com.hdfcbank.offerengine.domain.model.CustomerInquiryCriteria;
-import com.hdfcbank.offerengine.domain.model.OfferRecord;
+import com.hdfcbank.offerengine.domain.model.EligibleOfferRecord;
 
 import java.util.List;
 
 public interface OfferRecordRepositoryPort {
 
-    List<OfferRecord> findLiveMatches(CustomerInquiryCriteria criteria, int maxResults);
+    List<EligibleOfferRecord> findLiveMatches(CustomerInquiryCriteria criteria, int maxResults);
 }
