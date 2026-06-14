750c2905c8d2a1c85316781f7411c294437fafb7
Nilesh-Joshi26 <48581869+Nilesh-Joshi26@users.noreply.github.com>
2026-06-14 18:46:22 +0530

    Validate enhanced customer inquiry search keys

--- src/main/java/com/hdfcbank/offerengine/domain/service/CustomerInquiryDomainService.java
+++ src/main/java/com/hdfcbank/offerengine/domain/service/CustomerInquiryDomainService.java
@@ -2,7 +2,7 @@ package com.hdfcbank.offerengine.domain.service;
 
 import com.hdfcbank.offerengine.config.OfferEngineProperties;
 import com.hdfcbank.offerengine.domain.model.CustomerInquiryCriteria;
-import com.hdfcbank.offerengine.domain.model.OfferRecord;
+import com.hdfcbank.offerengine.domain.model.EligibleOfferRecord;
 import com.hdfcbank.offerengine.domain.port.in.CustomerInquiryUseCase;
 import com.hdfcbank.offerengine.domain.port.out.OfferRecordRepositoryPort;
 import org.springframework.stereotype.Service;
@@ -22,11 +22,13 @@ public class CustomerInquiryDomainService implements CustomerInquiryUseCase {
     }
 
     @Override
-    public List<OfferRecord> findEligibleOffers(CustomerInquiryCriteria criteria) {
+    public List<EligibleOfferRecord> findEligibleOffers(CustomerInquiryCriteria criteria) {
         if (isBlank(criteria.campaignName())) {
             throw new DomainValidationException("campaignName is mandatory");
         }
-        boolean noSearchKey = Stream.of(criteria.aan(), criteria.mobileNumber(), criteria.emailId(), criteria.merchantId())
+        boolean noSearchKey = Stream.of(
+                        criteria.aan(), criteria.mobileNumber(), criteria.emailId(), criteria.merchantId(),
+                        criteria.merchantName(), criteria.keyField3(), criteria.keyField4(), criteria.keyField5())
                 .allMatch(this::isBlank);
         if (noSearchKey) {
             throw new DomainValidationException("At least one search key must be supplied");
