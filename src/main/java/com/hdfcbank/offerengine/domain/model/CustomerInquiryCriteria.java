42e62958e72048f9024e7b568659c7310dbc9d40
Nilesh-Joshi26 <48581869+Nilesh-Joshi26@users.noreply.github.com>
2026-06-14 18:44:18 +0530

    Enhance customer inquiry criteria for multi merchant offers

--- src/main/java/com/hdfcbank/offerengine/domain/model/CustomerInquiryCriteria.java
+++ src/main/java/com/hdfcbank/offerengine/domain/model/CustomerInquiryCriteria.java
@@ -2,9 +2,14 @@ package com.hdfcbank.offerengine.domain.model;
 
 public record CustomerInquiryCriteria(
         String campaignName,
+        String labelCode,
         String aan,
         String mobileNumber,
         String emailId,
-        String merchantId
+        String merchantId,
+        String merchantName,
+        String keyField3,
+        String keyField4,
+        String keyField5
 ) {
 }
